9288de786f3a1f81f24568a0b1d5c3e2a5b53d54
urthbound <jeffowler@gmail.com>
2014-08-31 01:29:23 -0400

    mid infi

--- infinitelife.rb
+++ infinitelife.rb
@@ -1,6 +1,9 @@
-@cells = []
-
 def populate
+    cells = []
+    10.times do
+        cells << [rand(9),rand(9)]
+    end
+    return cells
 end
 
 def find_neighbors(coord)
@@ -11,18 +14,34 @@ def find_neighbors(coord)
     output.uniq
 end
 
-def tick(world)
-    old_world = Marshal.load(Marshal.dump(world))
-
-    new_world = []
-
-    world.each do |c|
-        neighbors = find_neighbors(c)
-        tally = (neighbors & world).length
-        if tally == 2 || tally == 3
-            new_world << c
-        end
-    end
+def try_dead_cell(cell, world)
+    neighbors = find_neighbors(cell)
+    tally = (neighbors & world).length
+    return true if tally == 3
 end
 
-p find_neighbors([0,0])
+def tick(world)
+    old_world = Marshal.load(Marshal.dump(world))
+    new_world = []
+
+    world.each do |living_cell|
+        neighbors = find_neighbors(living_cell)
+        dead_neighbors = neighbors - world
+
+        dead_neighbors.each do |c|
+            new_world << c if try_dead_cell(c, world)
+        end
+
+        tally = (neighbors & world).length
+        new_world << living_cell if tally == 2 || tally == 3
+    end
+    new_world.uniq
+end
+
+x = populate
+p x
+loop do 
+    p x = tick(x)
+    gets
+end
+
